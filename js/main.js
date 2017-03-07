4fa0f1258726704da3eb9d6b99dc2e9ab5d71ac7
Julio César Carrascal Urquijo <jcarrascal@gmail.com>
2017-03-06 19:30:48 -0500

    Added random questions

--- js/main.js
+++ js/main.js
@@ -9,6 +9,29 @@
         controls,
         clock;
 
+    var questionMesh;
+
+    var questionMesh,
+        answersMeshes = [],
+        questions = [
+        {
+            "question": "What...is your name?",
+            "answers": ["Sir Lancelot of Camelot", "Sir Tim Berners Lee", "Prince Ruppert"]
+        },
+        {
+            "question": "What...is your quest?",
+            "answers": ["To seek the Fountain of Youth", "To seek the Holy Grail", "The British are coming!"]
+        },
+        {
+            "question": "What...is your favorite color?",
+            "answers": ["Indigo", "Blue", "Fucshia!"]
+        },
+        {
+            "question": "What...is the capital of Assyria?",
+            "answers": ["Cairo", "Mesopotamia", "I don't know THAT!!"]
+        },
+    ];
+
     init();
 
     function init() {
@@ -79,6 +102,8 @@
         floor.rotation.x = -Math.PI / 2;
         scene.add(floor);
 
+        updateQuestion();
+
         // Start rendering.
         clock = new THREE.Clock();
         animate();
@@ -127,5 +152,78 @@
             container.webkitRequestFullscreen();
         }
     }
+
+    function updateQuestion() {
+        var index = parseInt(Math.random() * questions.length);
+        var item = questions[index];
+        if (questionMesh) {
+            scene.remove(questionMesh);
+        }
+        if (answersMeshes) {
+            for (var answerMesh of answersMeshes) {
+                scene.remove(answerMesh);
+            }
+            answersMeshes = [];
+        }
+
+        var questionGeometry = new THREE.TextGeometry(item.question, {
+            size: 2,
+            height: 0.1
+        });
+        questionMesh = new THREE.Mesh(questionGeometry, new THREE.MeshBasicMaterial({
+            color: 0x468966, opacity: 1
+        }));
+        questionMesh.position.y = 20;
+        questionMesh.position.z = 20;
+        questionMesh.rotation.x = 0;
+        questionMesh.rotation.y = -180;
+        scene.add(questionMesh);
+
+        var y = 15;
+        for (var answer of item.answers) {
+            var text = answer;
+            var answerGeometry = new THREE.TextGeometry(answer, {
+                size: 3,
+                height: 0.1
+            });
+            var answerMesh = new THREE.Mesh(answerGeometry, new THREE.MeshBasicMaterial({
+                color: 0xFFF0A5, opacity: 0.8
+            }));
+            answerMesh.position.y = y;
+            answerMesh.position.z = 20;
+            answerMesh.rotation.x = 0;
+            answerMesh.rotation.y = -180;
+            answerMesh.click = function () {
+                alert(text);
+            }
+            answersMeshes.push(answerMesh);
+            scene.add(answerMesh);
+            y -= 5;
+        }
+    }
+
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+
+    function onDocumentMouseDown(event) {
+
+        event.preventDefault();
+
+        mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+        mouse.y = - (event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+
+        raycaster.setFromCamera(mouse, camera);
+
+        var intersects = raycaster.intersectObjects(scene.children);
+
+        if (intersects.length > 0) {
+            if (intersects[0].object.click) {
+                intersects[0].object.click();
+            }
+        }
+
+    }
+
+    document.addEventListener('mousedown', onDocumentMouseDown);
 
 })();
